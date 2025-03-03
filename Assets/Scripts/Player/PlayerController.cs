414247392fad1af1d9c9ec33360f4105e0323193
chenmarcyong@gmail.com <chenmarcyong@gmail.com>
2025-03-03 19:17:55 +0100

    first commit

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,118 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+
+    public InputAction playerControls;
+    public float moveSpeed = 25.0f;
+
+    public InputActionReference move;
+    public InputActionReference ascend;
+    public InputActionReference rotate;
+
+    private Vector2 moveDirection = Vector2.zero;
+    private float ascendSpeed = 5.0f;
+    private Rigidbody rb;
+
+
+    public float maxThrustSpeed = 1.5f;
+    public float maxThrustEnergy = 100.0f;
+    public float thrustSpeed;
+    public float thrustEnergy;
+    private bool isThrusting;
+    private bool needsRestThrust;
+
+    public float verticalMove;
+    public float horizontalMove;
+    public float mouseInputX;
+    public float mouseInputY;
+    public float rollInput;
+
+    [SerializeField]
+    float speedMult = 1.0f;
+
+    [SerializeField]
+    float speedMultAngle = 0.5f;
+
+    [SerializeField]
+    float speedRollMultAngle = 0.05f;
+
+    void Start()
+    {
+        thrustSpeed = 1.0f;
+        thrustEnergy = maxThrustEnergy;
+        isThrusting = false;
+        needsRestThrust = false;
+
+        Cursor.lockState = CursorLockMode.Locked;
+        rb = GetComponent<Rigidbody>();
+        rb.useGravity = false;
+    }
+
+    void Update()
+    {
+        moveDirection = move.action.ReadValue<Vector2>();
+        ascendSpeed = ascend.action.ReadValue<float>();
+        //UnityEngine.Debug.Log(rb.position);
+
+        if (isThrusting && !needsRestThrust) 
+        {
+            thrustEnergy = Math.Max(thrustEnergy - 0.15f, 0.0f);
+            thrustSpeed = Math.Min(thrustSpeed * 1.005f, maxThrustSpeed);
+
+            if (thrustEnergy == 0.0f) needsRestThrust = true;
+
+
+        }
+        else
+        {
+            thrustEnergy = Math.Min(thrustEnergy + 0.075f, maxThrustEnergy);
+            thrustSpeed = Math.Max(thrustSpeed * 0.995f, 1.0f);
+
+            if (needsRestThrust && thrustEnergy >= 50.0f)
+            {
+                needsRestThrust = false;
+            }
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector3(-moveDirection.x * moveSpeed, ascendSpeed * moveSpeed, -moveDirection.y * moveSpeed * thrustSpeed);
+    }
+
+    /*public void OnMove() 
+    {
+        rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+    }*/
+    public void OnThrust(InputAction.CallbackContext context)
+    {
+        if (context.started) 
+        {
+            isThrusting = true;
+        }
+
+        else if (context.canceled)
+        {
+            isThrusting = false;
+        }
+    }
+
+    private void OnEnable()
+    {
+        playerControls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        playerControls.Disable();
+    }
+}
