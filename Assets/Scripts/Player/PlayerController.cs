68a8b5d2730f45e927816efa8ea13ae6b9e83b95
chenmarcyong@gmail.com <chenmarcyong@gmail.com>
2025-03-05 23:31:30 +0100

    Add Player

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,118 +1,70 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Diagnostics;
 using UnityEngine;
-using UnityEngine.InputSystem;
+using UnityEngine.InputSystem;  
 
 public class PlayerController : MonoBehaviour
 {
     // Start is called before the first frame update
 
+    public float moveSpeed = 5f;
+    public float jumpForce = 5f;
 
-    public InputAction playerControls;
-    public float moveSpeed = 25.0f;
-
-    public InputActionReference move;
-    public InputActionReference ascend;
-    public InputActionReference rotate;
-
-    private Vector2 moveDirection = Vector2.zero;
-    private float ascendSpeed = 5.0f;
     private Rigidbody rb;
+    private Vector2 moveInput;
+    private bool isGrounded;
 
 
-    public float maxThrustSpeed = 1.5f;
-    public float maxThrustEnergy = 100.0f;
-    public float thrustSpeed;
-    public float thrustEnergy;
-    private bool isThrusting;
-    private bool needsRestThrust;
+    void Awake() 
+    {
+        rb = GetComponent<Rigidbody>();
 
-    public float verticalMove;
-    public float horizontalMove;
-    public float mouseInputX;
-    public float mouseInputY;
-    public float rollInput;
-
-    [SerializeField]
-    float speedMult = 1.0f;
-
-    [SerializeField]
-    float speedMultAngle = 0.5f;
-
-    [SerializeField]
-    float speedRollMultAngle = 0.05f;
+    }
 
     void Start()
     {
-        thrustSpeed = 1.0f;
-        thrustEnergy = maxThrustEnergy;
-        isThrusting = false;
-        needsRestThrust = false;
-
-        Cursor.lockState = CursorLockMode.Locked;
-        rb = GetComponent<Rigidbody>();
-        rb.useGravity = false;
+        
     }
 
+    // Update is called once per frame
     void Update()
     {
-        moveDirection = move.action.ReadValue<Vector2>();
-        ascendSpeed = ascend.action.ReadValue<float>();
-        //UnityEngine.Debug.Log(rb.position);
-
-        if (isThrusting && !needsRestThrust) 
-        {
-            thrustEnergy = Math.Max(thrustEnergy - 0.15f, 0.0f);
-            thrustSpeed = Math.Min(thrustSpeed * 1.005f, maxThrustSpeed);
-
-            if (thrustEnergy == 0.0f) needsRestThrust = true;
-
-
-        }
-        else
-        {
-            thrustEnergy = Math.Min(thrustEnergy + 0.075f, maxThrustEnergy);
-            thrustSpeed = Math.Max(thrustSpeed * 0.995f, 1.0f);
-
-            if (needsRestThrust && thrustEnergy >= 50.0f)
-            {
-                needsRestThrust = false;
-            }
-        }
-
+        
     }
 
-    private void FixedUpdate()
+    void FixedUpdate()
     {
-        rb.velocity = new Vector3(-moveDirection.x * moveSpeed, ascendSpeed * moveSpeed, -moveDirection.y * moveSpeed * thrustSpeed);
+        Vector3 movement = new Vector3(moveInput.x, 0, moveInput.y) * moveSpeed;
+        rb.MovePosition(rb.position + movement * Time.fixedDeltaTime);
     }
 
-    /*public void OnMove() 
+
+
+    // ------------On Functions------------
+
+
+    public void OnMove(InputAction.CallbackContext context)
     {
-        rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
-    }*/
-    public void OnThrust(InputAction.CallbackContext context)
+        moveInput = context.ReadValue<Vector2>();
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
     {
-        if (context.started) 
+        if (context.performed && isGrounded)
         {
-            isThrusting = true;
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
         }
 
-        else if (context.canceled)
+        UnityEngine.Debug.Log("Jump");
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
         {
-            isThrusting = false;
+            isGrounded = true;
         }
-    }
-
-    private void OnEnable()
-    {
-        playerControls.Enable();
-    }
-
-    private void OnDisable()
-    {
-        playerControls.Disable();
+        //UnityEngine.Debug.Log("Grounded");
     }
 }
