3baa2f7893d1c8c05187bb03c24165764d22cf3c
chenmarcyong@gmail.com <chenmarcyong@gmail.com>
2025-03-17 01:19:54 +0100

    add spaceship_pc interaction

--- Assets/Scripts/SpaceShip/spaceship_pc/SpaceShip_pc_interaction.cs
+++ Assets/Scripts/SpaceShip/spaceship_pc/SpaceShip_pc_interaction.cs
@@ -1,17 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Diagnostics;
 using UnityEngine;
 
-public class SpaceShipInteraction : MonoBehaviour
+public class SpaceShip_pc_interaction : MonoBehaviour
 {
-    // Start is called before the first frame update
 
     public float interactionRadius = 2f; // Rayon d'interaction
     public string playerTag = "Player"; // Tag du joueur
     public Transform teleportPoint;
     public GameObject obj;
-
+    
+    // Start is called before the first frame update
     void Start()
     {
         
@@ -38,30 +37,29 @@ public class SpaceShipInteraction : MonoBehaviour
             //UnityEngine.Debug.Log("Appuie sur 'E' pour interagir avec le vaisseau !");
             if (Input.GetKeyDown(KeyCode.E))
             {
-                InteractWithSpaceship();
+                InteractWithPC();
             }
         }
     }
 
-    void InteractWithSpaceship()
+    void InteractWithPC()
     {
-        UnityEngine.Debug.Log("Le joueur interagit avec le vaisseau !");
+        UnityEngine.Debug.Log("Le joueur interagit avec le PC !");
         GameObject player = GameObject.FindGameObjectWithTag("Player");
         player.transform.position = teleportPoint.position;
         Quaternion targetRotation = teleportPoint.rotation;
 
         // Inverser uniquement l'axe Z
-        Quaternion newRotation = Quaternion.Euler(targetRotation.eulerAngles.x, targetRotation.eulerAngles.y, -targetRotation.eulerAngles.z);
+        Quaternion newRotation = Quaternion.Euler(targetRotation.eulerAngles.x, targetRotation.eulerAngles.y - 180, targetRotation.eulerAngles.z);
 
         // Appliquer la nouvelle rotation au joueur
-        player.transform.rotation = newRotation; 
-        // Ajoute ici les actions à effectuer (monter dans le vaisseau, ouvrir un menu, etc.)
+        player.transform.rotation = newRotation;// Ajoute ici les actions à effectuer (monter dans le vaisseau, ouvrir un menu, etc.)
     }
 
     // Affiche la sphère dans l'éditeur pour voir le rayon d'interaction
     void OnDrawGizmosSelected()
     {
-        Gizmos.color = Color.yellow;
+        Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(obj.transform.position, interactionRadius);
     }
 }
